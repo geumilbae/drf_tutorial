7ffa9e1c483eeaef8f0dd54aa43b85111c9c8481
geumilbae <geumil.bae@projectybl.com>
2020-04-15 09:11:31 +0900

    튜토리얼 공부를 마쳤습니다. 베스트프랙티스 공부하러 갑니다.
    2020년 4월 15일, 오늘은 총선 투표일입니다.

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """User 모델에 대한 CRUD 를 허용하는 API Endpoint 입니다."""
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """Group 모델에 대한 CRUD 를 허용하는 API Endpoint 입니다."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
